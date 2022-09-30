3b62e30336e55d6acba191afd237c84493cf8abf
SemiGodProgrammer <kacpik07@gmail.com>
2022-09-30 18:40:35 +0200

    Page update

---
+++ ASCII.js
@@ -0,0 +1,41 @@
+const density = "Ñ@#W$&9876543210?!aboc;:+=-,._";
+
+let video;
+let asciiDiv;
+
+function preload() {
+  video = loadImage("https://media.tenor.com/x8v1oNUOmg4AAAAM/rickroll-roll.gif");
+}
+function setup() {
+  noCanvas();
+  //video = createCapture(VIDEO);
+  video.resize(90, 90);
+  asciiDiv = createDiv();
+  asciiDiv.class("ascii");
+  asciiDiv.parent("main");
+}
+function draw() {
+  video.loadPixels();
+  let asciiImage = "";
+  for (let i = 0; i < video.height; i++) {
+    for (let j = 0; j < video.width; j++) {
+      let pixelIndex = (j + i * video.width) * 4;
+      let r = video.pixels[pixelIndex + 0];
+      let g = video.pixels[pixelIndex + 1];
+      let b = video.pixels[pixelIndex + 2];
+
+      let avg = (r + b + g) / 3;
+
+      const len = density.length;
+      const charIndex = floor(map(avg, 0, 255, len, 0));
+
+      const char = density.charAt(charIndex);
+      //const finalChar = `<span style='color:rgb(${r},${g},${b})'>${char}</span>`;
+      if (char == " ") asciiImage += "&nbsp;";
+      else asciiImage += char;
+    }
+    asciiImage += "<br/>";
+  }
+  asciiDiv.html(asciiImage);
+  image(video, 0, 0);
+}
---
+++ script.js
@@ -0,0 +1,126 @@
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+//Texture Loader
+const loader = new THREE.TextureLoader();
+const star = loader.load("./star.png");
+// Scene
+const scene = new THREE.Scene();
+
+// Objects
+const geometry = new THREE.BoxGeometry(1, 1, 1, 30, 30, 30);
+
+const particlesGeometry = new THREE.BufferGeometry();
+const particlesCnt = 5000;
+
+const posArr = new Float32Array(particlesCnt * 3);
+
+for (let i = 0; i < particlesCnt * 3; i++) {
+  posArr[i] = (Math.random() - 0.5) * (Math.random() * 5);
+}
+
+particlesGeometry.setAttribute("position", new THREE.BufferAttribute(posArr, 3));
+
+// Materials
+
+const material = new THREE.PointsMaterial({
+  size: 0.005,
+  color: "#99ffff",
+});
+
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.005,
+  map: star,
+  transparent: true,
+  color: "white",
+});
+
+// Mesh
+const sphere = new THREE.Points(geometry, material);
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(sphere, particles);
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff, 0.1);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 2;
+scene.add(camera);
+
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setClearColor(new THREE.Color("#232323"));
+
+//Mouse
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock();
+
+let color = 0;
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  sphere.rotation.y = 0.5 * elapsedTime;
+
+  sphere.material.color.r = Math.sin(elapsedTime);
+  sphere.material.color.g = Math.sin(elapsedTime * 0.8) + 0.2;
+
+  particles.rotation.y = elapsedTime * -0.1;
+  particles.rotation.x = 0.03 * elapsedTime;
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
